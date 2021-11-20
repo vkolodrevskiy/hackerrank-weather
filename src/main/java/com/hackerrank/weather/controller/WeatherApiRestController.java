1076e020bef63ad35372f5889f64d12d4ee142d3
Viktor Kolodrevskiy <viktor.kolodrevskiy@pexapark.com>
2021-11-20 20:24:23 +0200

    get by id

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -2,11 +2,14 @@ package com.hackerrank.weather.controller;
 
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.service.WeatherService;
+import java.util.Optional;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class WeatherApiRestController {
 
-  @Autowired
-  private WeatherService weatherService;
+  private final WeatherService weatherService;
+
+  public WeatherApiRestController(WeatherService weatherService) {
+    this.weatherService = weatherService;
+  }
 
   @PostMapping("/weather")
-  public ResponseEntity<Weather> createWeather(@Valid @RequestBody Weather weather) {
-      Weather createdWeather = weatherService.save(weather);
-      return new ResponseEntity<>(createdWeather, HttpStatus.CREATED);
+  public ResponseEntity<Weather> addWeather(@RequestBody @Valid Weather weather) {
+    Weather createdWeather = weatherService.save(weather);
+    return new ResponseEntity<>(createdWeather, HttpStatus.CREATED);
+  }
+
+  @GetMapping(value = "/weather/{id}")
+  public ResponseEntity<Weather> getWeatherById(@PathVariable @NotNull Integer id) {
+    Optional<Weather> weatherOp = weatherService.findById(id);
+    return weatherOp.map(weather -> ResponseEntity.status(HttpStatus.OK).body(weather))
+        .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
   }
 }
--- src/main/java/com/hackerrank/weather/service/WeatherService.java
+++ src/main/java/com/hackerrank/weather/service/WeatherService.java
@@ -2,20 +2,22 @@ package com.hackerrank.weather.service;
 
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.repository.WeatherRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherService {
+  private final WeatherRepository weatherRepository;
 
-  @Autowired
-  private WeatherRepository weatherRepository;
+  public WeatherService(WeatherRepository weatherRepository) {
+    this.weatherRepository = weatherRepository;
+  }
 
   public Weather save(Weather weather) {
     return weatherRepository.save(weather);
   }
 
-
-
-
+  public Optional<Weather> findById(Integer id) {
+    return weatherRepository.findById(id);
+  }
 }
